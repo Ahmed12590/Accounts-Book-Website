62d2dedf7946a6a222f81dfbd0a61d42b50254cf
unknown <itsharisofficial291@gmail.com>
2024-09-06 22:41:40 +0500

    php mailer and other

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 
-
+//on medium and small screen sidemenu
 var sidemenu = document.getElementById("sidemenu");
 
 function openmenu(){
@@ -14,4 +14,17 @@ sidemenu.style.right = "-200px";
 
 
 
+//calendly js
+window.onload = function() {
+    Calendly.initBadgeWidget({
+      url: 'https://calendly.com/info-accsbook/30min',text:'Schedule a free call',color: '#2F4680',
+      textColor: '#ffffff',width: '100%'});
+  }
 
+  //calendly calling on different link/buttons
+  var calendyBtn = document.getElementById('calendlyBtn');
+  calendyBtn.addEventListener("click", function() {
+  Calendly.initPopupWidget({ url: 'https://calendly.com/info-accsbook/30min' });
+    return false;
+    });
+            
