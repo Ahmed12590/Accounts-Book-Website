3c29bd730c1b46bef58e8b60abb857e86580780c
AfaqAhmd <afaqahmed468@gmail.com>
2024-09-06 23:05:38 +0500

    Update send-e copy.php

--- send-e copy.php
+++ send-e copy.php
@@ -24,7 +24,7 @@ try {
     $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
     $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
     $mail->Username   = 'afaqahmed468@gmail.com';                     //SMTP username
-    $mail->Password   = 'usfc ieyr wzpv yghu';                               //SMTP password
+    $mail->Password   = 'password';                               //SMTP password
     $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
     $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
 
